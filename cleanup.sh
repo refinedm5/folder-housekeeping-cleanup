f34f4b124f6d4049d9921ac932d587d4a44c1265
refinedm5 <demonspeedster@gmail.com>
2022-07-05 17:50:07 +0700

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@ logfile="/mnt/log/admin/cleanup.log"
 targetdir="/mnt/log/"
 startime=$(date)
 echo "========================================================================" >> $logfile
-echo "cleaning up archived catalina.out older than 3 days on $startime" >> $logfile
+echo "cleaning up archived files older than 3 days on $startime" >> $logfile
 for y in `find $workdir -type f -mtime +2 -name "catalina.out*"`
 do
 echo "deleting $y..." >> $logfile
@@ -20,4 +20,3 @@ done
 endtime=$(date)
 echo "Cleanup on $endtime has been completed" >> $logfile
 echo "========================================================================" >> $logfile
-
