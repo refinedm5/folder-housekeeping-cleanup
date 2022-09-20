00efb8fd5fbcb7abf3eb442580a44bf16644b638
refinedm5 <demonspeedster@gmail.com>
2022-09-20 09:10:45 +0700

    Add IFS variable configuration
    Change the value of IFS variable to handle filenames with spaces during for loop

--- housekeeping.sh
+++ housekeeping.sh
@@ -6,10 +6,11 @@ startime=$(date)
 echo "========================================================================" >> $logfile
 #set to 60 minutes, change -mmin +x accordingly
 echo "archiving compressed catalina.out on $startime" >> $logfile
+SAVEIFS=$IFS
+IFS=$'\n'
 for y in `find $workdir -type f -mmin +60 -name "catalina.out*gz"`
 do
 echo "archiving $y to /mnt/log/" >> $logfile
-echo "$y"
 cp --preserve $y $targetdir
 statx=$?
 if [ $statx -eq 0 ]
@@ -28,6 +29,7 @@ if [ $statx -eq 0 ]
                 echo "failed to archive $y, something went wrong" >> $logfile
 fi
 done
+SAVEIFS=$IFS
 endtime=$(date)
 echo "Housekeeping on $endtime has been completed" >> $logfile
 echo "========================================================================" >> $logfile
